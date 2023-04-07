8e816d1c463ffe800d75140a0f3460aff04b9165
gracebir <grace.devolop@gmail.com>
2023-04-07 22:01:54 +0200

    ajust mobile view

--- src/components/Offre.tsx
+++ src/components/Offre.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link';
 
 function Offre() {
     return (
-        <div className="mx-auto max-w-[1300px] pb-6 flex flex-col items-center gap-14 lg:items-end">
-            <h1 className="font-bold text-3xl text-center">Nos vous offrons</h1>
+        <div className="mx-auto max-w-[1300px] pb-6 flex flex-col items-center gap-14 lg:items-end my-16">
+            <h1 className="font-bold text-3xl text-center lg:mx-auto">Nos vous offrons</h1>
             <div className="bg-[#0000002e] flex flex-col lg:w-[760px] items-center text-[1.5rem] w-[460px] mx-auto text-center px-4 py-4 lg:h-14 lg:flex-row lg:justify-around rounded">
                 <p className="cursor-pointer font-semibold transition duration-300 ease-in hover:text-[#FC7F2A]">Gaz à GPL</p>
                 <p className="cursor-pointer font-semibold transition duration-300 ease-in hover:text-[#FC7F2A]">Gaz en Bouteille</p>
@@ -18,7 +18,7 @@ function Offre() {
                     return <Card key={items.id} {...items} />;
                 })}
             </div>
-            <Link className="border border-[#fc7f2a] float-left rounded text-[1.2rem] font-semibold text-[#fc7f2a] px-16 py-3" href="#">Voir +</Link>
+            <Link className="border border-[#fc7f2a] transition duration-300 ease-in hover:bg-[#fc7f2a] hover:text-white float-left rounded text-[1.2rem] font-semibold text-[#fc7f2a] px-16 py-3" href="#">Voir +</Link>
         </div>
     )
 }
